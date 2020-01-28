164b4b9fdfadc80c66954da4a8a12dd2bfd8fbd2
sam <samreality4@gmail.com>
2020-01-27 17:42:40 -0800

    update simon

--- game.js
+++ game.js
@@ -1,2 +1,79 @@
-alert("hello");
-$("h1").css("color", "blue");
+var randomNumber = "";
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColor = "";
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+startGame();
+
+$(".btn").click(function(e) {
+  var userClickedColor = String(e.target.id);
+  userClickedPattern.push(userClickedColor);
+  soundOnEvent(userClickedColor);
+  animatePress(userClickedColor);
+  checkAnswer();
+  //console.log(userClickedColor);
+});
+
+function startGame() {
+  $(document).one("keydown", function(e) {
+    randomChosenColor = nextSequence();
+    $("#level-title").text("Level " + level);
+  });
+}
+
+function nextSequence() {
+  randomNumber = Math.floor(3 * Math.random()) + 1;
+  randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  console.log(gamePattern);
+  soundOnEvent(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn();
+  level += 1;
+  return randomChosenColor;
+}
+
+function soundOnEvent(key) {
+  var audio = new Audio("sounds/" + key + ".mp3");
+  audio.play();
+}
+
+function animatePress(key) {
+  var id = "#" + key;
+  $(id).addClass("pressed");
+  setTimeout(function() {
+    $(id).removeClass("pressed");
+  }, 100);
+}
+
+//check the current userClickedPattern array vs current gamePattern array everytime a button is clicked.
+function checkAnswer() {
+  for (var i = 0; i < userClickedPattern; i++) {
+    if (userClickedPattern[i] != gamePattern[i]) {
+      soundOnEvent("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      }, 200);
+      $("#level-title").text("You lose! Press any key to restart!");
+      startOver();
+    }  else if (userClickedPattern[i] == gamePattern[i] &&
+      userClickedPattern.length == gamePattern.length) {
+      console.log(gamePattern);
+      console.log(userClickedPattern);
+      setTimeout(nextSequence, 1000);
+      $("#level-title").text("Level " + level);
+      userClickedPattern = [];
+    }
+    
+  }
+}
+
+function startOver() {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+}
